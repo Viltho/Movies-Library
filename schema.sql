08a16cbcf7c50a60672229a926dee5b7d8a4ac00
Abdullah Abu Hamad <abdullahabuhammed@gmail.com>
2023-03-13 22:20:52 +0300

    new updates

--- schema.sql
+++ schema.sql
@@ -3,6 +3,7 @@ DROP TABLE IF EXISTS favmovies;
 CREATE TABLE IF NOT EXISTS favmovies (
     id SERIAL PRIMARY KEY,
     movie_title VARCHAR(255),
-    movie.movie_title VARCHAR(255),
-    summary VARCHAR(10000)
+    release_date VARCHAR(255),
+    poster_path VARCHAR(255),
+    overview VARCHAR(10000)
 );
